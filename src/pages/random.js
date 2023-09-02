99e124a2f39be5ff1ff497441d0d7294126bd7c8
mrosendo <mrosendo@leadtech.com>
2023-09-02 14:50:01 +0200

    Start

---
+++ src/pages/random.js
@@ -0,0 +1,177 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Image from "next/image";
+import {
+  questions,
+  historic,
+  expectations,
+  election,
+} from "@/questions/questions";
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  gap: 50px;
+`;
+
+const FlipCard = styled.div`
+  background-color: transparent;
+  width: 300px;
+  height: 500px;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+`;
+
+const FlipCardInner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+  transform:${props => props.show ? 'rotateY(180deg)' : null};
+`;
+
+const CardFront = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  border-radius: 30px;
+  background: linear-gradient(
+    45deg,
+    rgba(70, 11, 13, 1) 0%,
+    rgba(190, 32, 40, 1) 100%
+  );
+  background: linear-gradient(
+    45deg,
+    rgba(108, 12, 12, 1) 0%,
+    rgba(255, 73, 83, 1) 100%
+  );
+
+  position: absolute;
+  backface-visibility: hidden;
+`;
+
+const CardTexture = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  min-height: 470px;
+  width: 270px;
+  border-radius: 15px;
+  background-image: url("/curveTexture.jpg");
+  background-size: cover;
+  mix-blend-mode: multiply;
+`;
+
+const CardColor = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 470px;
+  width: 270px;
+  border-radius: 15px;
+  background: linear-gradient(
+    45deg,
+    rgba(9, 10, 10, 1) 5%,
+    rgba(90, 45, 47, 1) 100%
+  );
+  img {
+    z-index: 0;
+  }
+`;
+
+const CardBack = styled(CardFront)`
+  background-color: white;
+  color: black;
+  transform: rotateY(180deg);
+  color: black;
+`;
+
+const BackWhite = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  border-radius: 15px;
+  min-height: 470px;
+  width: 270px;
+  background-color: white;
+`;
+
+const BackTexture = styled(CardTexture)`
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
+  background-color: white;
+  background-image: url("/whiteTexture.jpg");
+  background-size: cover;
+  mix-blend-mode: multiply;
+`;
+
+const BackContent = styled.div`
+  color: black;
+  z-index:10;
+  font-size:24px;
+  padding:30px;
+`;
+
+const Button = styled.button`
+  border-radius:25px;
+  min-height:50px;
+  color:white;
+  background:linear-gradient(180deg, rgba(129,23,25,1) 0%, rgba(190,32,40,1) 100%);
+  font-size:24px;
+  min-width:300px;
+  border: 0px;
+
+`;
+
+const random = () => {
+  const [question, setQuestion] = useState("Click next to get your question");
+  const [show, setShow] = useState(false);
+
+  const randomQuestion = () => {
+    setShow(false);
+    setTimeout(()=>{
+        const allQuestions = questions
+      .concat(historic)
+      .concat(expectations)
+      .concat(election);
+    const randomIndex = Math.floor(allQuestions.length * Math.random());
+    setQuestion(allQuestions[randomIndex]);
+    setShow(true);
+    },1000);
+    
+  };
+
+  return (
+    <Layout>
+      <FlipCard>
+        <FlipCardInner show={show}>
+          <CardFront onClick={randomQuestion}>
+            <CardColor>
+              <CardTexture />
+              <Image src="/logo.png" width={200} height={194} />
+            </CardColor>
+          </CardFront>
+          <CardBack onClick={randomQuestion}>
+            <BackWhite />
+            <BackTexture />
+            <BackContent>{question}</BackContent>
+          </CardBack>
+        </FlipCardInner>
+      </FlipCard>
+      <Button onClick={randomQuestion}>Next Question</Button>
+    </Layout>
+  );
+};
+
+export default random;
